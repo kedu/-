93e7898ec855c14053418e45ecc4ac967391df70
lkb <1218773641@qq.com>
2016-06-23 07:49:50 +0800

    提交第一次代码

---
+++ 第一次测试/第一次测试/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  第一次测试
+//
+//  Created by Apple on 16/6/23.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"我是最棒的-----------------------------");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
